175d2cefea1122b6e94dba1c2c0d8a67a1d2321c
Jesús García Crespo <jesus@sevein.com>
2025-06-11 13:51:27 +0200

    Add tests, linting and improved types

--- pygfried/__init__.pyi
+++ pygfried/__init__.pyi
@@ -1,11 +1,44 @@
-from typing import Union, Literal, Any
+from typing import Literal
+from typing import TypedDict
+from typing import overload
 
-class GoError: ...
+class GoError(Exception):
+    """Exception raised when Go code encounters an error."""
 
-SimpleIdentifyResult = Union[Literal["UNKNOWN"], str, None]
-DetailedIdentifyResult = Any
+SimpleIdentifyResult = Literal["UNKNOWN"] | str | None
 
-def identify(
-    path: str, detailed: bool = False
-) -> Union[SimpleIdentifyResult, DetailedIdentifyResult]: ...
+class Match(TypedDict):
+    ns: str
+    id: str
+    format: str
+    version: str
+    mime: str
+    basis: str
+    warning: str
+
+Match.__annotations__["class"] = str
+
+class File(TypedDict):
+    filename: str
+    filesize: int
+    modified: str
+    errors: str
+    matches: list[Match]
+
+class Identifier(TypedDict):
+    name: str
+    details: str
+
+class DetailedIdentifyResult(TypedDict):
+    siegfried: str
+    scandate: str
+    signature: str
+    created: str
+    identifiers: list[Identifier]
+    files: list[File]
+
+@overload
+def identify(path: str, detailed: Literal[True]) -> DetailedIdentifyResult: ...
+@overload
+def identify(path: str, detailed: Literal[False] = False) -> SimpleIdentifyResult: ...
 def version() -> str: ...
